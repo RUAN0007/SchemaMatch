44cda5a008f9ed5f11531b5089478d4c4cf1ab94
RUAN0007 <ruanpingcheng@gmail.com>
2016-10-26 09:46:07 +0800

    Seperate each job into independent questions

--- src/Crowdsourcing.cpp
+++ src/Crowdsourcing.cpp
@@ -3,41 +3,65 @@
 #include <stdlib.h>     /* srand, rand */
 #include <time.h>       /* time */
 #include "debug.h"
+string tpQuestionDir = "tp_questions/";
+string tpAnswerDir = "tp_answers/";
+string tpInfoPath = tpQuestionDir + "tp_info.json";
+string smQuestionDir = "sm_questions/";
+string smInfoPath = smQuestionDir + "sm_info.json";
+string smAnswerDir = "sm_answers/";
 
 int Crowdsourcing::postColTypeCorrespondece(const map<string,vector<URI>>& candidateCorrespondece,
 											const WebTable& wt) const {
 
 	//Deal with case that has not candidate correspondences
 
+	Json::Value infoRoot;
+	Json::Reader reader;
+
+	ifstream tpInfoFile(tpInfoPath, ifstream::binary);
+	if(tpInfoFile.good()){
+
+		if(tpInfoFile.is_open() && !reader.parse(tpInfoFile, infoRoot)){
+			LOG(LOG_FATAL, "Fail to parse %s as a json file. ", tpInfoPath.c_str());
+		}
+	}
+	srand(time(NULL));
+	int jobID = rand() % 1000; //JobID is a random integer from [0,999]
+
+	Json::Value questionInfoNode;
 	//Write the question to file so that a python program can read and post the question
 	Json::Value wtNode = wt.serialize();
 
-	Json::Value colNode;
-	Json::Value corrNode;
 	for(const auto& corr: candidateCorrespondece) {
+
+		int questionID = rand() % 9000 + 1000; //QuestionID is a random integer from [1000,9999]
+		questionInfoNode.append(questionID);
 		string col = corr.first;
 		vector<URI> types = corr.second;
 
-		colNode.append(col);
+		Json::Value questionRoot;
+		Json::Value corrNode;
 		Json::Value typesNode;
 		for(const URI& type:types) {
 			typesNode.append(type);
 		}//End of for URI
 		corrNode[col] = typesNode;
+		questionRoot["correspondence"] = corrNode;
+		questionRoot["webtable"] = wtNode;
+
+		ofstream tpQuestionFile;
+		tpQuestionFile.open(tpQuestionDir + "TP" + to_string(questionID) + ".json",std::ios::out);
+		tpQuestionFile << questionRoot.toStyledString();
+		tpQuestionFile.close();
 	}
 
-	Json::Value root;
-	root["webtable"] = wtNode;
-	root["cols"] = colNode;
-	root["correspondence"] = corrNode;
-
-
-	srand(time(NULL));
-	int jobID = rand() % 1000; //select a random 4-digit integer as a job ID
-	ofstream tpQuestionFile;
-	tpQuestionFile.open("cs_questions/" + to_string(jobID) + ".json",std::ios::out);
-	tpQuestionFile << root.toStyledString();
-	tpQuestionFile.close();
+	//cout <<questionInfoNode.toStyledString() << endl;
+	infoRoot[to_string(jobID)] = questionInfoNode;
+//	cout << infoRoot.toStyledString() << endl;
+	ofstream infoFile;
+	infoFile.open(tpInfoPath,std::ios::out);
+	infoFile << infoRoot.toStyledString();
+	infoFile.close();
 	return jobID;
 
 }
@@ -50,7 +74,7 @@ map<string, URI> Crowdsourcing::getColTypeCorrespondence(int jobID) const{
 //	correspondence["default_table_1"] = "https://www.comp.nus.edu.sg/todo_url#Country";
 //	correspondence["default_table_2"] = "https://www.comp.nus.edu.sg/todo_url#Capital";
 
-	string fpath = "cs_answers/" + to_string(jobID) + ".json";
+	string fpath = tpAnswerDir + to_string(jobID) + ".json";
 	ifstream answerFile(fpath, ifstream::binary);
 	if(!answerFile.is_open()) {
 		LOG(LOG_FATAL,"%s does not exist.",fpath.c_str() );
@@ -79,32 +103,55 @@ map<string, URI> Crowdsourcing::getColTypeCorrespondence(int jobID) const{
 
 int Crowdsourcing::postColMatching(const map<string, vector<string>>& candidateMatching, const WebTable& wt1, const WebTable& wt2) const{
 	//Write question to file so that a python program can later read
+	Json::Value infoRoot;
+
+	Json::Reader reader;
+
+	ifstream smInfoFile(smInfoPath, ifstream::binary);
+	if(smInfoFile.good()){
+
+		if(!reader.parse(smInfoFile, infoRoot)){
+			LOG(LOG_FATAL, "Fail to parse %s as a json file. ", tpInfoPath.c_str());
+		}
+	}
+	srand(time(NULL));
+	int jobID = rand() % 1000; //select a random 4-digit integer as a job ID
+
 	Json::Value wt1Node = wt1.serialize();
 	Json::Value wt2Node = wt2.serialize();
-	Json::Value colNode;
-	Json::Value matchingsNode;
+
+	Json::Value questionInfoNode;
 	for(const auto& match: candidateMatching) {
-		string col1 = match.first;
-		colNode.append(col1);
-		vector<string> matchCols = match.second;
+		int questionID = rand() % 9000 + 1000; //QuestionID is a random integer from [1000,9999]
+		questionInfoNode.append(questionID);
+
+		Json::Value questionRoot;
+		Json::Value matchingNode;
 		Json::Value matchColNode;
+
+		string col1 = match.first;
+		vector<string> matchCols = match.second;
 		for(string matchCol:matchCols) {
 			matchColNode.append(matchCol);
 		}
-		matchingsNode[col1] = matchColNode;
+		matchingNode[col1] = matchColNode;
+
+		questionRoot["wt1"] = wt1Node;
+		questionRoot["wt2"] = wt2Node;
+		questionRoot["match"] = matchingNode;
+
+		ofstream smQuestionFile;
+		smQuestionFile.open(smQuestionDir + "SM" + to_string(questionID) + ".json",std::ios::out);
+		smQuestionFile << questionRoot.toStyledString();
+		smQuestionFile.close();
 	}
 
-	Json::Value root;
-	root["wt1"] = wt1Node;
-	root["wt2"] = wt2Node;
-	root["cols"] = colNode;
-	root["matchings"] = matchingsNode;
-	srand(time(NULL));
-	int jobID = rand() % 1000; //select a random 4-digit integer as a job ID
-	ofstream smQuestionFile;
-	smQuestionFile.open("cs_questions/" + to_string(jobID) + ".json",std::ios::out);
-	smQuestionFile << root.toStyledString();
-	smQuestionFile.close();
+	infoRoot[to_string(jobID)] = questionInfoNode;
+
+	ofstream smInfoOutputFile;
+	smInfoOutputFile.open(smInfoPath,std::ios::out);
+	smInfoOutputFile << infoRoot.toStyledString();
+	smInfoOutputFile.close();
 	return jobID;
 }
 
@@ -115,7 +162,7 @@ map<string,string> Crowdsourcing::getColMatching(int jobID) const{
 	//for testing purpose
 //	matching["WTA_1"] = "WTB_0";
 //	matching["WTB_2"] = "N.A.";
-	string fpath = "cs_answers/" + to_string(jobID) + ".json";
+	string fpath = smAnswerDir + to_string(jobID) + ".json";
 	ifstream answerFile(fpath, ifstream::binary);
 	if(!answerFile.is_open()) {
 		LOG(LOG_FATAL,"%s does not exist.",fpath.c_str() );
