f2bec37b8bf4ae7556113c9a4f5b54d88aa0bce9
RUAN0007 <ruanpingcheng@gmail.com>
2016-10-26 19:39:35 +0800

    Finish table pattern with timon

---
+++ config.py
@@ -0,0 +1,14 @@
+timon_url = "http://localhost:8000"
+server_url = "http://localhost:9000"
+callback_url = "http://localhost:9000/callback"
+job_id=1
+questionInfoPath = "tp_questions/tp_info.json"
+questionDirPath = "tp_questions"
+questionFinishPath = "tp_finish"
+
+finalAnswerPath = 'tp_answers'
+
+tempAnswerPath = "cs_temp/tp_answers.json"
+postedQuestionPath = "cs_temp/tp_posted_questions.json"
+debug=True
+port=9000
