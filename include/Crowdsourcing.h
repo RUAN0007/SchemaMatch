80059044f63047b1704adb14ceafe465ef59df7b
RUAN0007 <ruanpingcheng@gmail.com>
2016-12-01 16:33:52 +0800

    Add jobID to post question API

--- include/Crowdsourcing.h
+++ include/Crowdsourcing.h
@@ -6,25 +6,27 @@ using namespace std;
 #include "KnowledgeBase.h"
 #include "WebTable.h"
 #include "json.h"
+#include "dice_cs.h"
 
 class Crowdsourcing {
 public:
-
+	static int CSID;
 	/*
 	 * Post question regarding the schema matching
 	 * Args:
+	 * 	jobID
 	 * 	candidateMatching:
 	 * 		key: a colID in wt1
 	 * 		value: a vector of ColIDs in wt2 which are possible matches for the wt1's colID in the key
 	 * 	wt1,wt2: Webtable for the schema matching
 	 *
 	 * Return:
-	 * 	the ID of this crowdsourcing job
+	 * 	whether the question posting is successful or not.
 	 */
 
-	int postColMatching(const map<string, vector<string>>& candidateMatching,
+	bool postColMatching(int jobID, const map<string, vector<string>>& candidateMatching,
 							const WebTable& wt1,
-							const WebTable& wt2) const;
+							const WebTable& wt2) ;
 
 	/*
 	 * Get the result column matching based on the job ID
@@ -33,6 +35,11 @@ public:
 	 * 	key: col ID in wt1
 	 * 	value:col ID in wt2
 	 */
-	map<string,string> getColMatching(int jobID) const;
+	map<string,string> getColMatching(int jobID);
 
+	Crowdsourcing(dice_cs* csConnector) {
+		this->genericCS = csConnector;
+	}
+private:
+	dice_cs* genericCS;
 };
